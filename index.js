9c557566ef03d693eabdccb761c47a95d5443d74
Zilong Guan <cerealbox269@gmail.com>
2025-04-16 02:35:09 -0400

    Completed a working toggle for share, still need refining

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world');
+const openShareBtn = document.getElementById('open-share');
+const closeShareBtn = document.getElementById('close-share');
+const popUp = document.querySelector('.popup');
+
+openShareBtn.addEventListener('click', () => {
+  popUp.style.display = 'flex';
+});
+
+closeShareBtn.addEventListener('click', () => {
+  popUp.style.display = 'none';
+});
