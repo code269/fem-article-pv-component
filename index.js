1f2205b66bbc427419be0609713379eee97e025a
Zilong Guan <cerealbox269@gmail.com>
2025-07-01 17:23:40 -0400

    Completed working invert button click

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const openShareBtn = document.getElementById('open-share');
 const closeShareBtn = document.getElementById('close-share');
 const popUp = document.querySelector('.popup');
+const buttonImg = document.querySelector('.profile__share-button');
 
 openShareBtn.addEventListener('click', () => {
   popUp.classList.toggle('visible');
+  openShareBtn.classList.toggle('button--dark');
+  buttonImg.classList.toggle('button__img--dark');
 });
 
 closeShareBtn.addEventListener('click', () => {
   popUp.classList.toggle('visible');
+  openShareBtn.classList.toggle('button--dark');
+  buttonImg.classList.toggle('button__img--dark');
 });
